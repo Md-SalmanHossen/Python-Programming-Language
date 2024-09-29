2cb3a17996d8a6eabf44fd85bd92a1b9d4b07f56
SalmanHossen <hossainsalman873@gmail.com>
2024-09-29 13:48:29 +0600

    practice datatype

---
+++ 01Variable_DataType/2variable.py
@@ -0,0 +1,9 @@
+
+name ="Salman Hossen"
+age=23
+gpa=3.00
+
+print("my name is :",name)
+print("my age is :",age)
+print("my gpa is :",gpa)
+
---
+++ 01Variable_DataType/3DataType.py
@@ -0,0 +1,12 @@
+
+name ="Salman Hossen"
+age=23
+gpa=3.00
+
+print("my name is :",name)
+print("my age is :",age)
+print("my gpa is :",gpa)
+
+print(type(name))
+print(type(age))
+print(type(gpa))
